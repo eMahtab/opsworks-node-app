de60c2b4e01383dce0cce1c459928a3ef4a5032e
Mahtab Alam <alammahtab08@gmail.com>
2018-10-16 22:23:08 +0530

    Changed back to v1

--- server.js
+++ server.js
@@ -3,15 +3,8 @@ var http = require("http");
 var port = process.env.PORT || 80;
 
 http.createServer(function (request, response) {
-
-   // Send the HTTP header 
-   // HTTP Status: 200 : OK
-   // Content Type: text/plain
    response.writeHead(200, {'Content-Type': 'text/html'});
-   
-   // Send the response body as "Hello World"
-   response.end("<h1 align='center'>Heyyo v3 :)</h1>");
+   response.end("<h1 align='center'>Heyyo v1 :)</h1>");
 }).listen(port);
 
-// Console will print the message
 console.log('Server running at http://127.0.0.1:'+port);
