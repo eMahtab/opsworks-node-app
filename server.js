11173d6134dc1f23dfef014309b9c09f5d67a210
Mahtab Alam <alammahtab08@gmail.com>
2018-10-16 22:51:30 +0530

    Changed to v2

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var port = process.env.PORT || 80;
 
 http.createServer(function (request, response) {
    response.writeHead(200, {'Content-Type': 'text/html'});
-   response.end("<h1 align='center'>Heyyo v1 :)</h1>");
+   response.end("<h1 align='center'>Heyyo v2 :)</h1>");
 }).listen(port);
 
 console.log('Server running at http://127.0.0.1:'+port);
